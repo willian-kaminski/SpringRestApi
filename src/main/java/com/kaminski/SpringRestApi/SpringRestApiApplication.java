07c4bc3338a15db9a69ca2da41a43e3c1643aeec
willian-kaminsnki <developer.williansantos@gmail.com>
2020-09-06 21:55:05 -0300

    first commit

---
+++ src/main/java/com/kaminski/SpringRestApi/SpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.kaminski.SpringRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaminski/SpringRestApi/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminski.SpringRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
